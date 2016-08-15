88bb4ab2b45f3ddf61c4d4f115d7676e08fc575b
Casey Cabrales <caseycabrales@gmail.com>
2016-08-14 19:11:20 -0700

    Added all opcodes in switch statement. Still need to fully implement a couple of them (TODO).

--- js/chip8.js
+++ js/chip8.js
@@ -66,6 +66,11 @@ class Chip8 {
     cycle() {
         // Able to read as a single number when joining them
         let opCode = this.memory[this.pc] << 8 | this.memory[this.pc + 1];
+        let x = (opCode & 0x0F00) >> 8;
+        let y = (opCode & 0x00F0) >> 4;
+
+        // Increment pc by 2 and decrement sound and delay
+        this.pc += 2;
 
         // Read the first portion to classify the code, then can nest further for more classification
         switch (opCode & 0xF000) {
@@ -73,45 +78,282 @@ class Chip8 {
 
                 switch (opCode) {
                     // CLS - Clear Screen
+                        // clear renderer and reset display
                     case 0x00E0:
-
+                        this.renderer.clear();
+                        for (let i = 0; i < this.display.length; i++) {
+                            this.display[i] = 0;
+                        }
                         break;
 
                     // RET - Return from subroutine
                     case 0x00EE:
+                        this.pc = stack[--this.sp];
                         break;
 
                 }
                 break;
+
+            // JP address
+                // jump to address 1NNN
             case 0x1000:
+                this.pc = opCode & 0xFFF;
                 break;
+
+            // CALL subroutine
+                // call at 2NNN
             case 0x2000:
+                this.stack[this.sp] = this.pc;
+                this.sp++;
+                this.pc = opCode & 0xFFF;
                 break;
+
+            // SE 3XNN
+                // skip next instruction if Vx == NN
             case 0x3000:
+                if (this.v[x] === (opCode & 0xFF)) {
+                    this.pc += 2;
+                }
                 break;
+
+            // SNE 4XNN
+                // skip next instruction if Vx != NN
             case 0x4000:
+                if (this.v[x] !== (opCode & 0x00FF)) {
+                    this.pc += 2;
+                }
                 break;
+
+            // SE 5XY0
+                // skips next instruction if Vx === Vy
             case 0x5000:
+                if (this.v[x] === this.v[y]) {
+                    this.pc += 2;
+                }
                 break;
+
+            // LD 6XNN
+                // sets Vx to NN
             case 0x6000:
+                this.v[x] = (opCode & 0xFF);
                 break;
+
+            // ADD 7XNN
+                // add NN to Vx
             case 0x7000:
+                let calc = this.v[x] + (opCode & 0xFF);
+                // Prevent overflow
+                if (calc > 255) {
+                    calc -= 256;
+                }
+                this.v[x] = calc;
                 break;
+
             case 0x8000:
+                switch (opCode & 0x000F) {
+                    // LD 8XY0
+                        // set Vx to Vy
+                    case 0x0000:
+                        this.v[x] = this.v[y];
+                        break;
+
+                    // OR 8XY1
+                        // set Vx to Vx OR Vy
+                    case 0x0001:
+                        this.v[x] |= this.v[y];
+                        break;
+
+                    // AND 8XY2
+                        // set Vx to Vx AND Vy
+                    case 0x0002:
+                        this.v[x] &= this.v[y];
+                        break;
+
+                    // XOR 8XY3
+                        // set Vx to Vx XOR Vy
+                    case 0x0003:
+                        this.v[x] ^= this.v[y];
+                        break;
+
+                    // ADD 8XY4
+                        // adds Vy to Vx. VF is set to 1 when there's a carry, 0 otherwise
+                    case 0x0004:
+                        this.v[x] += this.v[y];
+                        this.v[0xF] = +(this.v[x] > 255);
+
+                        if (this.v[x] > 255) {
+                            this.v[x] -= 256;
+                        }
+                        break;
+
+                    // SUB 8XY5
+                        // subtract Vy from Vx. VF is set to 0 when there's a borrow, 0 otherwise
+                    case 0x0005:
+                        this.v[0xF] = +(this.v[x] > this.v[y]);
+                        this.v[x] -= this.v[y];
+                        if (this.v[x] < 0) {
+                            this.v[x] += 256;
+                        }
+                        break;
+
+                    // SHR 8XY6
+                        // shift Vx right by one. VF is set to least significant bit of Vx before the shift
+                    case 0x0006:
+                        this.v[0xF] = this.v[x] & 0x1;
+                        this.v[x] >>= 1;
+                        break;
+
+                    // SUBN 8XY7
+                        // set Vx to Vy - Vx. VF is set to 0 when there's a borrow, 1 otherwise
+                    case 0x0007:
+                        this.v[0xF] = +(this.v[x] > this.v[y]);
+                        this.v[x] = this.v[y] = this.v[x];
+                        if (this.v[x] < 0) {
+                            this.v[x] += 256;
+                        }
+                        break;
+
+                    // SHL 8XYE
+                        // shift Vx left by one. VF set to most significant bit of Vx before shift
+                    case 0x000E:
+                        this.v[0xF] = this.v[x] & 0x8;
+                        this.v[x] <<= 1;
+                        break;
+                }
                 break;
+
+            // SNE 9XY0
+                // skips next instruction if Vx != Vy
             case 0x9000:
+                if (this.v[x] !== this.v[y]) {
+                    this.pc += 2;
+                }
                 break;
+
+            // LD I ANNN
+                // sets I to address NNN
             case 0xA000:
+                this.i = opCode & 0xFFF;
                 break;
+
+            // JP V0 BNNN
+                // jumps to address NNN + V0
             case 0xB000:
+                this.pc = (opCode & 0xFFF) + this.v[0];
                 break;
+
+            // RND CXNN
+                // set Vx to result of AND on a random number and NN
             case 0xC000:
+                this.v[x] = Math.floor(Math.random() * 0xFF) & (opCode & 0xFF);
                 break;
+
+            // DRW DXYN
+                // draws a sprite at coordinate (Vx, Vy) with a width of 8 pixels and height of N pixels
+                // each row of 8 pixels is read as a bit-coded starting from memory location I
+                // VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, 0 otherwise
+            // TODO
             case 0xD000:
+                let width = 8;
+                let height = opCode & 0x000F;
+                this.v[0xF] = 0;
+
+                // for (let )
                 break;
+
             case 0xE000:
+                switch(opCode & 0x00FF) {
+
+                    // SKP EX9E
+                        // skips next instruction if the key stored in Vx is pressed
+                    case 0x009E:
+                        if (this.keys[this.v[x]]) {
+                            this.pc += 2;
+                        }
+                        break;
+
+                    // SKNP EXA1
+                        // skips next instruction if the key stored in Vx is not pressed
+                    case 0x00A1:
+                        if (!this.keys[this.v[x]]) {
+                            this.pc += 2;
+                        }
+                        break;
+                }
                 break;
+
             case 0xF000:
+                switch(opCode & 0x00FF) {
+
+                    // LD FX07
+                        // set Vx to value of delay timer
+                    case 0x0007:
+                        this.v[x] =  this.delayTimer;
+                        break;
+
+                    // LD FX0A
+                        // wait for key press, then store in Vx
+                        //TODO
+                    case 0x000A:
+                        let oldKey = this.setKey;
+                        this.setKey = (key) => {
+                            this.v[x] = key;
+                        }
+                        break;
+
+                    // LD FX15
+                        // sets delay timer to Vx
+                    case 0x0015:
+                        this.delayTimer = this.v[x];
+                        break;
+
+                    // LD FX18
+                        // sets sound timer to Vx
+                    case 0x0018:
+                        this.soundTimer = this.v[x];
+                        break;
+
+                    // ADD I FX1E
+                        // adds Vx to I
+                    case 0x001E:
+                        this.i += this.v[x];
+                        break;
+
+                    // LD F FX29
+                        // sets I to the location of the sprite for the character in Vx
+                        // characters 0-F are represented by a 4x5 font
+                    case 0x0029:
+                        this.i = this.v[x] * 5;
+                        break;
+
+                    // LD B FX33
+                        // stores binary representation of Vx in memory, with the most significant of three digits
+                        // at addr in I, middle digit at I+1, LSB at I+2
+                    case 0x0033:
+                        var dec = this.v[x];
+
+                        for (let i = this.i + 2; i >= this.i - 2; i--) {
+                            this.memory[i] = parseInt(dec % 10);
+                            dec /= 10;
+                        }
+                        break;
+
+                    // LD [I] FX55
+                        // stores V0 to Vx (inclusive) in memory address starting at address I
+                    case 0x0055:
+                        for (let i = 0; i <= x; i++) {
+                            this.memory[this.i + i] = this.v[i];
+                        }
+                        break;
+
+                    // LD Vx FX65
+                        // fills V0 to Vx (inclusive) with values from memory starting at address I
+                    case 0x0065:
+                        for (let i = 0; i <= x; i++) {
+                            this.v[i] = this.memory[this.i + i];
+                        }
+                        break;
+                }
                 break;
             default:
                 throw new Error("Invalid opcode " + opCode.toString(16) + " received. Quitting now.");
