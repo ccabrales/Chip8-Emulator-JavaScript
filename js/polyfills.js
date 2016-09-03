7a347abb4c1b6d92e433e47876408982436fdd0e
Casey Cabrales <caseycabrales@gmail.com>
2016-09-03 14:09:51 -0700

    Added polyfill and blip sound

---
+++ js/polyfills.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Casey on 9/3/16.
+ */
+
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 0);
+        };
+})();
+
+Function.prototype.bind = Function.prototype.bind || function(context) {
+    var boundArgs = [].slice.call(arguments, 1);
+    var boundFn = this;
+
+    return function() {
+        return boundFn.apply(context, boundArgs.concat([].slice.call(arguments)));
+    };
+};
