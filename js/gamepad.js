2aad667ebea627c6392d85d3951e512dd088ab4b
Casey Cabrales <caseycabrales@gmail.com>
2016-09-03 12:07:01 -0700

    First run at gamepad class

--- js/gamepad.js
+++ js/gamepad.js
@@ -2,4 +2,32 @@
  * Created by Casey on 8/15/16.
  */
 
-//TODO
+class Gamepad {
+    constructor(callback) {
+        if (!callback || typeof callback != 'function') {
+            throw new Error("You must specify a callback to implement");
+        }
+        this.callback = callback;
+        this.running = false;
+    }
+
+    start() {
+        if (!Gamepad.supported) return;
+
+        this.running = true;
+        this.step();
+    }
+
+    stop() {
+        this.running = false;
+    }
+
+    step() {
+        if (!this.running) return;
+
+        this.callback(navigator.webkitGetGamepads());
+        webkitRequestAnimationFrame(this.step.bind(this));
+    }
+}
+
+Gamepad.supported = !!navigator.webkitGetGamepads;
